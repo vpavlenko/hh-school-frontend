5e76398dce3c4008310031535cd97c9d8f4b3c1a
vpavlenko <cxielamiko@gmail.com>
2013-09-15 23:06:04 +0400

    simple add implemented

--- impl/js/calendar.js
+++ impl/js/calendar.js
@@ -3,27 +3,74 @@ $(document).ready(function() {
 
     moment.lang("ru");
 
-    var events;
+    /*
+     * We assume that there's only one event per day.
+     * So the 'events' list should have only one entry per day.
+     */
+    var events = [];
+
     var currentMonth;
 
+    // called once in init(), see below
     function mockEvents() {
         var mocks = [{title: "Митинг победителей",
-                      date: new Date(2013, 8, 9),
-                      participants: ["Леонид Волков", "Алексей Навальный"],
+                      date: moment({year: 2013, month: 8, day: 9}),
+                      participants: "Леонид Волков, Алексей Навальный",
                       description: "Митинг сторонников Навального на Болотной площади"
+                     },
+                     {title: "Дедлайн по вступительной в HH",
+                      date: moment({year: 2013, month: 8, day: 15}),
+                      participants: ["Виталий Павленко"],
+                      description: "Задание тут: http://school.hh.ru/#form"
+                     },
+                     {title: "Hallowe'en",
+                      date: moment({year: 2013, month: 9, day: 31}),
+                      participants: "Ghosts, vampires",
+                      description: "Может, в этом году таки отметить?"
                      }
                     ];
         events = mocks;
+    }
+
+    function saveToLocalStorage() {
+        localStorage.setItem('events', JSON.stringify(events));
+    }
+
+    function restoreFromLocalStorage() {
+        events = JSON.parse(localStorage.getItem('events'));
+        for (var i in events) {
+            events[i].date = moment(events[i].date);
+        }
+        return events !== undefined;
     }
 
     function datesEqual(x, y) {
         return x.year() == y.year() && x.month() == y.month() && x.date() == y.date();
     }
 
+    function getEventByDate(date) {
+        for (var i in events) {
+            var event_ = events[i];
+            if (datesEqual(event_.date, date)) {
+                return event_;
+            }
+        }
+
+        return undefined;
+    }
+
+    function getTableEntryByEvent(event_) {
+        var entry = $('<div class="table-event">');
+        entry.append($('<div class="table-event-title">').text(event_.title));
+        entry.append($('<div class="table-event-participants">').text(event_.participants));
+        return entry;
+    }
+
     function drawGrid(startDate, endDate) {
         var table = $('<table>');
         var tr;
         var first_week = true;
+        var today = moment();
 
         for (var date = startDate; !datesEqual(date, endDate); date.add('day', 1)) {
             if (date.day() === 1) {
@@ -31,7 +78,18 @@ $(document).ready(function() {
             }
 
             var title = (first_week ? (date.format('dddd') + ', ') : '') + date.format('D');
-            tr.append($('<td>').html($('<span class="cell-title">').text(capitalizeFirstLetter(title))));
+            var td = $('<td>').html($('<div class="cell-title">').text(capitalizeFirstLetter(title)));
+            if (datesEqual(date, today)) {
+                td.addClass('today');
+            }
+
+            var currentDateEvent = getEventByDate(date);
+            if (currentDateEvent !== undefined) {
+                td.append(getTableEntryByEvent(currentDateEvent));
+                td.addClass('hasEvent');
+            }
+
+            tr.append(td);
 
             if (date.day() === 0) {
                 table.append(tr);
@@ -73,8 +131,64 @@ $(document).ready(function() {
         gotoMonth(moment());
     }
 
+    function addEvent(date, title) {
+        var newEvent = {
+            date: date,
+            title: title,
+            participants: '',
+            description: ''
+        };
+
+        var found = false;
+        for (var i in events) {
+            if (datesEqual(events[i].date, date)) {
+                events[i] = newEvent;
+                found = true;
+            }
+        }
+
+        if (!found) {
+            events.push(newEvent);
+        }
+
+        saveToLocalStorage();
+    }
+
+    function editEvent(date) {
+        // TODO: implement
+    }
+
+    function addQuickEvent(text) {
+        var parts = splitByComma(text);
+
+        if (parts.length == 0) {
+            return;
+        }
+        if (parts.length == 1) {
+            parts.push('');
+        }
+        if (parts.length > 2) {
+            parts[1] = parts.slice(1, parts.length).join(', ');
+            parts.length = 2;
+        }
+
+        var date = moment(parts[0], 'DD MMMM');
+        date.year(moment().year());
+
+        addEvent(date, parts[1]);
+        gotoMonth(date);
+        editEvent(date);
+    }
+
     function init() {
-        mockEvents();
+        if (!restoreFromLocalStorage()) {
+            mockEvents();
+        }
+
+        $('#button-add').click(function() {
+            $('#quick-add').modal({backdrop: false});
+            $('#quick-add-text').focus();
+        });
 
         $('#button-refresh').click(function() {
             window.location.reload();
@@ -90,6 +204,11 @@ $(document).ready(function() {
 
         $('#button-today').click(function() {
             gotoToday();
+        });
+
+        $('#quick-add-button').click(function() {
+            $('#quick-add .close').click();
+            addQuickEvent($('#quick-add-text').val());
         });
 
         gotoToday();
--- impl/js/utils.js
+++ impl/js/utils.js
@@ -1,4 +1,13 @@
-function capitalizeFirstLetter(string)
-{
-    return string.charAt(0).toUpperCase() + string.slice(1);
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function splitByComma(str) {
+    var str_array = str.split(',');
+
+    for (var i = 0; i < str_array.length; i++) {
+            str_array[i] = str_array[i].replace(/^\s*/, "").replace(/\s*$/, "");
+    }
+
+    return str_array;
 }
