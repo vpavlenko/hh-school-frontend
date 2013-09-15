64de55c487e54ed0f0d3a6e222b0c97fd6197c8d
vpavlenko <cxielamiko@gmail.com>
2013-09-15 19:05:57 +0400

    grid without event logic

--- impl/js/calendar.js
+++ impl/js/calendar.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     moment.lang("ru");
 
     var events;
+    var currentMonth;
 
     function mockEvents() {
         var mocks = [{title: "Митинг победителей",
@@ -15,27 +16,84 @@ $(document).ready(function() {
         events = mocks;
     }
 
-    function drawGrid() {
-
+    function datesEqual(x, y) {
+        return x.year() == y.year() && x.month() == y.month() && x.date() == y.date();
     }
 
-    function gotoMonth(month, year) {
-        var momentObj = moment({month: month, year: year});
-        $('#month').text(capitalizeFirstLetter(momentObj.format("MMMM YYYY")));
+    function drawGrid(startDate, endDate) {
+        var table = $('<table>');
+        var tr;
+        var first_week = true;
+
+        for (var date = startDate; !datesEqual(date, endDate); date.add('day', 1)) {
+            if (date.day() === 1) {
+                tr = $('<tr>');
+            }
+
+            var title = (first_week ? (date.format('dddd') + ', ') : '') + date.format('D');
+            tr.append($('<td>').html($('<span class="cell-title">').text(capitalizeFirstLetter(title))));
+
+            if (date.day() === 0) {
+                table.append(tr);
+                first_week = false;
+            }
+        }
+        $('#month-div').html(table);
     }
 
-    function gotoDay(day) {
-        gotoMonth(day.month(), day.year());
+    function gotoMonth(day) {
+        currentMonth = day;
+
+        $('#month').text(capitalizeFirstLetter(currentMonth.format("MMMM YYYY")));
+
+        var firstDateOfMonth = currentMonth.clone().date(1);
+        var lastDateOfMonth = currentMonth.clone().add('months', 1).date(0);
+
+        var startDate;
+        var endDate;
+
+        // calendar will represent dates in range [startDate; endDate)
+
+        if (firstDateOfMonth.day() === 0) {
+            startDate = firstDateOfMonth.clone().day(-6);
+        } else {
+            startDate = firstDateOfMonth.clone().day(1);
+        }
+
+        if (lastDateOfMonth.day() === 0) {
+            endDate = lastDateOfMonth.clone().add('day', 1);
+        } else {
+            endDate = lastDateOfMonth.clone().day(7).add('day', 1);
+        }
+
+        drawGrid(startDate, endDate);
     }
 
     function gotoToday() {
-        gotoDay(moment());
+        gotoMonth(moment());
     }
 
     function init() {
         mockEvents();
+
+        $('#button-refresh').click(function() {
+            window.location.reload();
+        });
+
+        $('#button-prev-month').click(function() {
+            gotoMonth(currentMonth.clone().add('month', -1));
+        });
+
+        $('#button-next-month').click(function() {
+            gotoMonth(currentMonth.clone().add('month', 1));
+        });
+
+        $('#button-today').click(function() {
+            gotoToday();
+        });
+
         gotoToday();
     }
 
     init();
-})
+});
